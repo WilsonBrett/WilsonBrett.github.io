02705cf890e76f580f87f97407a212a6fdd17cb3
butchbrody <brettwilson7@yahoo.com>
2015-10-21 12:36:14 -0400

    query_ui_bouncy

---
+++ unit_4/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#logoLeft img').click(function() {
+		$('#logoLeft').effect('bounce',{times:2},500);
+	});
+	
+	$('#logoRight img').click(function() {
+		$('#logoRight').effect('bounce',{times:2},500);
+	});
+
+});
