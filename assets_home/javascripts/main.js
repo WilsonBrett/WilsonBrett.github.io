488240dc06d5d4665b5a9082fe0556803ccd4f97
Brett Wilson <brettwilson7@yahoo.com>
2016-04-08 14:19:55 -0500

    animations added to about section

--- assets_home/javascripts/main.js
+++ assets_home/javascripts/main.js
@@ -1,6 +1,23 @@
 $(function(){
 
 	//console.log('ready!');
+	//document.scroll
+		//if window bottom is greater than about section offset.top then 
+			//decrease opacity of about section content to fade in
+			//may have to change the css to a different class containing an animation
+	var finishLine = false;
 
+	$(document).scroll(function() {
+
+		var bottom = $(window).height() + $(window).scrollTop();
+		var aboutTop = $('#about').offset().top;
+
+		if((bottom  >= aboutTop) && !finishLine) {
+			finishLine = true;
+			//console.log('window bottom is below about section top');
+			$('#about div').children().animate({opacity:1, left: '0px'}, 1250);
+		}
+
+	});
 
 });
